feef16eaadba12eaab5770ddbc3be3e1ce221a3b
adarsh946 <adarsh.shukla2724@gmail.com>
2025-02-03 23:38:05 +0530

    added clerk and database setup

--- components/ConvexClientProvider.tsx
+++ components/ConvexClientProvider.tsx
@@ -1,10 +1,9 @@
+"use client";
 import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 
-const convex = new ConvexReactClient(
-  process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!
-);
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export default function ConvexClientProvider({
   children,
